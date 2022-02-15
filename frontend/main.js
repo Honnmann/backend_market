7b9f1eae4a06615bfdb13e902e3be414a698a2b8
Honnmann <swtarhss07@gmail.com>
2022-02-15 01:35:25 -0500

    frontend

--- frontend/main.js
+++ frontend/main.js
@@ -103,7 +103,7 @@ __webpack_require__.r(__webpack_exports__);
 // The list of file replacements can be found in `angular.json`.
 const environment = {
     production: false,
-    API_URL: "http://localhost:8080"
+    API_URL: "http://35.225.35.19"
 };
 /*
  * For easier debugging in development mode, you can import the following file
